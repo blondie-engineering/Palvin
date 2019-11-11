c4e70744c32cca19c26a28c36aa211ae036a0b74
Paweł Walczak <walczakp007@gmail.com>
2019-11-11 23:10:21 +0100

    Add kafka setup

---
+++ data/README.md
@@ -0,0 +1,2 @@
+Download kafka binary (2.11-2.13) from here: https://kafka.apache.org/downloads
+Execute the shell script and your kafka producer is up and running
---
+++ data/setupKafka.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#download kafka to data directory
+tar -xzf kafka_2.11-2.3.0.tgz
+cd kafka_2.11-2.3.0
+bin/zookeeper-server-start.sh config/zookeeper.properties
+bin/kafka-server-start.sh config/server.properties
+bin/kafka-console-producer.sh --broker-list localhost:9092 --topic adData < ./transactions.txt
+export KAFKA_URL=localhost:9092
+export TOPIC=adData
+
