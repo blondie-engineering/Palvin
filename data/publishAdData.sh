2e867901d38b0daaba055718490546e129adc1ff
Paul Walczak <walczakp007@gmail.com>
2019-11-12 13:28:54 +0100

    Update publishAdData.sh

--- data/publishAdData.sh
+++ data/publishAdData.sh
@@ -4,7 +4,8 @@ iterations=$1
 while [ $iterations -ge 0 ]
 do
    echo Creating AdData - iteration: $iterations
-   python3 generateAdData.py
+   sleep $2
+   python3 generateAdData.py $3 $4
    bin/kafka-console-producer.sh --broker-list localhost:9092 --topic adData < ./transactions.txt
    ((iterations=iterations-1))
 done
