0c83ef1b31e51e5af29ca4c89bb8e220f892b937
Paul Walczak <walczakp007@gmail.com>
2019-11-12 13:32:43 +0100

    Update setupKafka.sh

--- data/setupKafka.sh
+++ data/setupKafka.sh
@@ -4,6 +4,7 @@ tar -xzf kafka_2.11-2.3.0.tgz
 cd kafka_2.11-2.3.0
 bin/zookeeper-server-start.sh config/zookeeper.properties
 bin/kafka-server-start.sh config/server.properties
+python3 generateAdData.py
 bin/kafka-console-producer.sh --broker-list localhost:9092 --topic adData < ./transactions.txt
 export KAFKA_URL=localhost:9092
 export TOPIC=adData
