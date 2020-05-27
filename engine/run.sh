415181b76da22822adb2dd3a48218cfff7d9edae
WalczakPaulo <walczakp007@gmail.com>
2020-05-27 19:59:37 +0200

    Fix run script

--- engine/run.sh
+++ engine/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 export KAFKA=localhost:9092
 export TOPIC=adData
-
+export IS_LOCAL=false
+export AWS_ACCESS_KEY_ID=xxx
+export AWS_SECRET_ACCESS_KEY=xxx
+export AWS_REGION=xxx
 buildProject="sbt build"
 runProject="sbt run"
 
